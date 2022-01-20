be933f54229303db1e5051ce8e3fbf9a01737553
Arthur Chabole <83458500+Chabole@users.noreply.github.com>
2022-01-20 16:40:45 -0300

    Revert "Add função sub"
    This reverts commit ec9a91b88e76fbe5e0de78760e26bab7bf94d78b.

--- Area_51.py
+++ Area_51.py
@@ -1,6 +1,5 @@
 print('1 modifição do arquivo')
 print("ola")
-
 def func():
     print('2 modifição do arquivo')
     return None
@@ -8,5 +7,10 @@ def func():
 def soma(a, b):
     return a + b
 
-def sub(a, b):
-    return a - b
+<<<<<<< HEAD
+print('Realidade paralela 1º')
+
+print('Realidade paralela 2º')
+=======
+print('Realidade paralela 1º')
+>>>>>>> parent of 2863ee7 (Add print 2 mod)
